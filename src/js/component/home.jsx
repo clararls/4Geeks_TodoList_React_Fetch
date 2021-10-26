9ec2cf8abec7dd552a8a19b5c21ae2f4b6e3cb8e
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-10-26 19:03:11 +0000

    working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,14 +3,29 @@ import Task from "./task.jsx";
 
 //create your first component
 const Home = () => {
-	const [taskList, setTaskList] = useState(["comer", "cocinar", "beber"]);
+	const [taskList, setTaskList] = useState([]);
+
+	//const deleteElement = index => {
+	//	props.setTaskList(props.taskList.filter((item, i) => i !== index));
+	//};
+
 	return (
 		<div className="d-flex justify-content-center flex-column">
 			<h1 className="text-center mt-5">TODOLIST</h1>
-			<input type="text" className="mx-auto text-info w-50 p-3" />
+			<input
+				type="text"
+				className="mx-auto text-info w-50 p-3"
+				onKeyDown={event => {
+					if (event.key === "Enter") {
+						setTaskList([...taskList, event.target.value]);
+					}
+				}}
+			/>
+
 			{taskList.map((item, i) => {
 				return <Task key={i} content={item} />;
 			})}
+			<p className="text-center mt-5">{taskList.length} items left</p>
 		</div>
 	);
 };
